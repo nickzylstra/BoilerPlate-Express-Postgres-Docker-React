906c8e77aef80fe17a50f0743c91115948e7a41f
nickzylstra <nick.zylstra@gmail.com>
2020-03-04 09:38:50 -0800

    Add security defaults with helmet

--- src/server/app.js
+++ src/server/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const helmet = require('helmet');
 const customersRouter = require('./routes/customers');
 
 const app = express();
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
